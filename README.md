0692256fdac1eee6fcbabe088e29fc35ee6e8d79
Mireiawen Rose <mir@mireiawen.net>
2019-09-21 22:26:44 +0200

    Add methods to get the data as specific data types and add documentation

--- README.md
+++ README.md
@@ -1,3 +1,183 @@
-# Configuration
 
-JSON configuration file helper class
+# Configuration
+JSON configuration file helper
+
+* Class name: `Configuration`
+* Namespace: `Mireiawen\Configuration`
+
+## Requirements
+* JSON extension
+* PHP 7
+
+## Methods
+
+### __construct
+    Configuration::__construct(string $filename)
+
+Configuration class constructor
+
+#### Arguments
+* **string** `$filename`  - The JSON file name to read
+
+#### Exceptions thrown
+##### \Exception
+* In case the extension is missing
+* In case the configuration file is not readable
+* In case of JSON errors
+
+### Get
+    mixed Configuration::Get(string $key, mixed $default)
+
+Get the configuration variable and return its value, or default if it is not set
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **mixed** `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **mixed** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+
+### GetString
+    string Configuration::GetString(string $key, string|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and validating the type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **string|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **string** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+##### \TypeError
+* In case the value is not of type string
+
+### GetInt
+    int Configuration::GetInt(string $key, int|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and validating the type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **int|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **int** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+##### \TypeError
+* In case the value is not of type int
+
+### GetFloat
+    float Configuration::GetFloat(string $key, float|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and validating the type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **float|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **float** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+##### \TypeError
+* In case the value is not of type float
+
+### GetBool
+    bool Configuration::GetBool(string $key, bool|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and validating the type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **bool|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **bool** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+##### \TypeError
+* In case the value is not of type bool
+
+### GetAsString
+    string Configuration::GetAsString(string $key, string|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and casting the return value to correct type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **string|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **string** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+### GetAsInt
+    int Configuration::GetAsInt(string $key, int|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and casting the return value to correct type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **int|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **int** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+### GetAsFloat
+    float Configuration::GetAsFloat(string $key, float|null $default)
+
+Get the configuration variable and return its value, or default if it is not set, and casting the return value to correct type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **float|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **float** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
+
+### GetAsBool
+    bool Configuration::GetAsBool(string key, bool|null default)
+
+Get the configuration variable and return its value, or default if it is not set, and casting the return value to correct type
+
+#### Arguments
+* **string** `$key` - The key to retrieve
+* **bool|null** - `$default` - The default value for the key, set to `NULL` to throw error if key is not set
+
+#### Return value
+* **bool** - the value of the key, or the provided default if key is not set
+
+#### Exceptions thrown
+##### \Exception
+* In case the key is not available and default value is not set
--- includes/Configuration.php
+++ includes/Configuration.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 namespace Mireiawen\Configuration;
 
 /**
- * Configuration helper class
+ * JSON configuration file helper
  *
  * @package Mireiawen\Configuration
  */
@@ -18,7 +18,7 @@ class Configuration
 	protected $data;
 	
 	/**
-	 * Config constructor.
+	 * Config constructor
 	 *
 	 * @param string $filename
 	 *    The JSON file name to read
@@ -42,14 +42,20 @@ class Configuration
 	}
 	
 	/**
-	 * Get the configuration variable and return its value, or default if it is not set
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set
 	 *
 	 * @param string $key
+	 *    The key to retrieve
+	 *
 	 * @param mixed $default
+	 *    The default value for the key, NULL to throw error if key is not set
 	 *
 	 * @return mixed
+	 *    The value of the key
 	 *
 	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
 	 */
 	public function Get(string $key, $default = NULL)
 	{
@@ -64,5 +70,209 @@ class Configuration
 		}
 		
 		return $default;
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and validating the type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param string|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return string
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 *
+	 * @throws \TypeError
+	 *    If the key value is of wrong type
+	 */
+	public function GetString(string $key, ?string $default = NULL) : string
+	{
+		$value = $this->Get($key, $default);
+		if (!\is_string($value))
+		{
+			throw new \TypeError(\sprintf(\_('Expected value of type %s, got %s'), 'string', \gettype($value)));
+		}
+		
+		return $value;
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and validating the type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param int|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return int
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 *
+	 * @throws \TypeError
+	 *    If the key value is of wrong type
+	 */
+	public function GetInt(string $key, ?int $default = NULL) : int
+	{
+		$value = $this->Get($key, $default);
+		if (!\is_int($value))
+		{
+			throw new \TypeError(\sprintf(\_('Expected value of type %s, got %s'), 'int', \gettype($value)));
+		}
+		
+		return $value;
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and validating the type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param float|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return float
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 *
+	 * @throws \TypeError
+	 *    If the key value is of wrong type
+	 */
+	public function GetFloat(string $key, ?float $default = NULL) : float
+	{
+		$value = $this->Get($key, $default);
+		if (!\is_float($value))
+		{
+			throw new \TypeError(\sprintf(\_('Expected value of type %s, got %s'), 'float', \gettype($value)));
+		}
+		
+		return $value;
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and validating the type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param bool|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return bool
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 *
+	 * @throws \TypeError
+	 *    If the key value is of wrong type
+	 */
+	public function GetBool(string $key, ?bool $default = NULL) : bool
+	{
+		$value = $this->Get($key, $default);
+		if (!\is_bool($value))
+		{
+			throw new \TypeError(\sprintf(\_('Expected value of type %s, got %s'), 'bool', \gettype($value)));
+		}
+		
+		return $value;
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and casting the return value to correct type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param string|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return string
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 */
+	public function GetAsString(string $key, ?string $default = NULL) : string
+	{
+		return (string)$this->Get($key, $default);
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and casting the return value to correct type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param int|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return int
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 */
+	public function GetAsInt(string $key, ?int $default = NULL) : int
+	{
+		return (int)$this->Get($key, $default);
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and casting the return value to correct type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param float|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return float
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 */
+	public function GetAsFloat(string $key, ?float $default = NULL) : float
+	{
+		return (float)$this->Get($key, $default);
+	}
+	
+	/**
+	 * Get the configuration variable and return its value, or default if it
+	 * is not set, and casting the return value to correct type
+	 *
+	 * @param string $key
+	 *    The key to retrieve
+	 *
+	 * @param bool|null $default
+	 *    The default value for the key, NULL to throw error if key is not set
+	 *
+	 * @return bool
+	 *    The value of the key
+	 *
+	 * @throws \Exception
+	 *    If the key is not set and no default value is specified
+	 */
+	public function GetAsBool(string $key, ?bool $default = NULL) : bool
+	{
+		return (bool)$this->Get($key, $default);
 	}
 }
